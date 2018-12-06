6a53212ec749421e345b81dc10482c8f739c1655
Adam Preston <ap720@kent.ac.uk>
2018-12-06 14:07:36 +0000

    completed final post request, added axios module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
-var bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const axios = require('axios');
 
 app.use(cors());
 
@@ -21,9 +21,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 let db = new sqlite3.Database('./FloodMonitoring.db');
 
+
+//-----GetSensorDetails------
+
+
 app.post('/GetSensorDetails', (req, res, next) =>
 {
-  let sql = 'SELECT hardware_serial_station_id, latitude, longitude FROM SensorDetails';
+  let sql = 'SELECT sensor_id, latitude, longitude FROM SensorDetails';
 
   db.all(sql, [], (err, rows) => {
   if (err) {
@@ -34,7 +38,7 @@ app.post('/GetSensorDetails', (req, res, next) =>
 
   //This is test - This should be the result or to be sent
   rows.forEach((row) => {
-    console.log(row.hardware_serial_station_id);
+    console.log(row.sensor_id);
   });
 
   });
@@ -42,10 +46,14 @@ app.post('/GetSensorDetails', (req, res, next) =>
   db.close();
 });
 
+
+//-----GetMostRecentFloodWarnings------
+
+
 app.post('/GetMostRecentFloodWarnings', (req, res, next) =>
 {
   //get sensor details to obtain all sensor ids (from db)
-  let sql1 = 'SELECT hardware_serial_station_id FROM SensorDetails';
+  let sql1 = 'SELECT sensor_id FROM SensorDetails';
 
   let floodWarningList = [];
   let sensor_ids = [];
@@ -63,12 +71,12 @@ sensor_ids.forEach((sensor_id) =>
 {
   let sql2 = 'SELECT * FROM FloodStatus WHERE sensor_id = ? ORDER BY DESC LIMIT 1';
 
-  db.all(sql2, [sensor_id.hardware_serial_station_id], (err, rows2) => {
+  db.get(sql2, [sensor_id.sensor_id], (err, floodAlert) => {
     if (err) {
       throw err;
     }
 
-  let sensorFloodStatus = {sensor_id: row.hardware_serial_station_id, severity_level: rows2[0].severity_level};
+  let sensorFloodStatus = {sensor_id: sensor_id.sensor_id, severity_level: floodAlert.severity_level};
   floodWarningList.push(sensorFloodStatus);
 });
 });
@@ -77,11 +85,14 @@ res.send(floodWarningList);
 
 });
 
+
+
+//-----GetLast24HoursOfData------
+
+
+
 app.post('/GetLast24HoursOfData', (req, res, next) =>
 {
-  //var sensor_id = req.body.sensor_id; NEED TO INCLUDE TEST
-
-  //find last 24 hours of data according to the sensor_id given.
   let sql = 'SELECT * FROM HistoricalData WHERE datetime <= datetime("now","-1 day") AND sensor_id = "lairdc0ee400001012345"';
   //object format: sensor24Data {sensor_id: sensor_id, water_height: value, date: date};
   db.all(sql, [], (err, rows) => {
@@ -95,20 +106,63 @@ app.post('/GetLast24HoursOfData', (req, res, next) =>
 
 });
 
+
+//-----GetCurrentValue------
+
+
 app.post('/GetCurrentValue', (req, res, next) =>
 {
   var sensor_id = req.body.sensor_id;
+  let sql = 'SELECT * FROM SensorDetails WHERE sensor_id = ? ORDER BY DESC LIMIT 1';
+  let isMQTT = false;
+  let latestSensorReading = -1;
   //use sensor id to find if sensor is MQTT or not
+  db.get(sql, [sensor_id], (err, row) => 
+  {
+    if(altitude != null)
+    {
+      isMQTT = true;
+    }
+  });
 
-
-  //if (sensor_id is MQTT)
+  if (isMQTT)
+  {
     //get lastest reading from most recent value in database for that sensor
-  //else
-    //(sensor is gov api)
-    //get latest reading by calling api
-  //res.send(latest sensor reading)
+    sql = "SELECT value_mm FROM HistoricalData WHERE sensor_id = ? ORDER BY DESC LIMIT 1";
+  
+    db.get(sql, [sensor_id], (err, row)=>
+    {
+      latestSensorReading = row;
+    });
+
+  }else
+  {
+    //get latest value for sensor by calling the gov api.
+    try
+    {
+      latestReading = getLatestReadingGov();
+      latestSensorReading = latestReading.items.value;
+      latestSensorReading * 1000;
+    }catch(error)
+    {
+      console.error(error);
+    }
+  }
+    
+  res.send(latestSensorReading)
 });
 
+//API REQUESTS
+
+const getLatestReadingGov = async () => {
+  try {
+    return await axios.get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/readings?latest')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+//PORT STUFF
 
 const port = process.env.PORT || 3000;
 
